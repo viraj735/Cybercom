041913a158b208fd9b52d9ef1b59f4f2c2f111a9
Viraj Ganatra <viraj.ganatra.vg@gmail.com>
2021-01-08 11:48:25 +0530

    JS File in variable & dataType Added

---
+++ Code/VariableAndDataType/script.js
@@ -0,0 +1,25 @@
+var sportName = "Hockey";
+console.log("Sports Name : "+sportName);
+
+var playerName = "Sir DhyanChand";
+var legend = true;
+console.log("Player Name : "+playerName);
+console.log("Is he a Legend : "+legend);
+
+var _age;
+console.log("Age :"+_age);
+
+var nullEx = "";
+console.log(nullEx);
+
+console.log("Let's define the undefined variable");
+
+_age = 32;
+console.log("Age :"+_age);
+
+var $country = "India";
+console.log("Country : "+$country);
+
+console.log(nullEx);
+
+console.log("Here we implemented all the dataTypes and variable declaration possibilities in JS ... ");
